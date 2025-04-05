6b27843282c7cbc0a3f058ecd8ff390c4feb948d
Dilara Ucar <dilara.ucaaar@gmail.com>
2025-04-05 21:01:33 +0000

    Add some changes to home page and updated styles with animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,27 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("current-year").textContent = new Date().getFullYear();
 });
+
+
+// Select all elements with the class 'fade-slide-up'
+const fadeElements = document.querySelectorAll('.fade-slide-up');
+
+// Function to check if the element is in the viewport
+function checkVisibility() {
+    fadeElements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+
+        // Check if element is within the viewport
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            element.classList.add('visible'); // Add the 'visible' class to trigger the fade-slide-up
+        } else {
+            element.classList.remove('visible'); // Remove the 'visible' class if not in viewport
+        }
+    });
+}
+
+// Run the function initially to check if elements are already in view
+checkVisibility();
+
+// Add event listener for scroll event
+window.addEventListener('scroll', checkVisibility);
